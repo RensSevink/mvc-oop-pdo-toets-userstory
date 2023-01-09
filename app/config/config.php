25157551e3c06ffeafdd0033f0a2ae7d753067e4
RensSevink <332726@student.mboutrecht.nl>
2023-01-09 12:38:22 +0100

    eleventh commit

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,6 @@ define('DB_NAME', 'BE-J2-P2-toets');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 
-define('URLROOT', 'http://www.mvc-oop-pdo-toets-userstory.com');
+define('URLROOT', 'http://mvc-oop-pdo-toets-userstory.com');
 
 define('SITENAME', 'MVC Framework Toets');
--- app/models/Mankement.php
+++ app/models/Mankement.php
@@ -9,13 +9,13 @@ class Mankement
     }
     public function getMankement()
     {
-        $this->db->query("SELECT Mankement.Id, Mankement.AutoId, Mankement.Datum, Mankement.Mankement, Instructeur.Naam AS INNA, Instructeur.Email AS EM, Auto.Kenteken AS AK, Auto.Type AS AT
-        FROM Mankement 
-        INNER JOIN Auto
-        ON Mankement.AutoId = Auto.Id
-        INNER JOIN Instructeur
-        ON Auto.InstructeurId = Instructeur.Id
-        WHERE Instructeur.Id = 2;");;
+        $this->db->query("SELECT Mankement.Id, Mankement.AutoId, Mankement.Datum, Mankement.Mankement, Instructeur.Naam, Instructeur.Email, Auto.Kenteken, Auto.Type
+                          FROM Mankement 
+                          INNER JOIN Auto
+                          ON Mankement.AutoId = Auto.Id
+                          INNER JOIN Instructeur
+                          ON Auto.InstructeurId = Instructeur.Id
+                          WHERE Instructeur.Id = 2;");;
         $result = $this->db->resultSet();
         return $result;
     }
