02d09049b9e386567013f388ffdaabfbcb3eb015
RensSevink <332726@student.mboutrecht.nl>
2023-01-09 13:06:52 +0100

    final commit

--- app/views/mankement/addMankement.php
+++ app/views/mankement/addMankement.php
@@ -1,7 +1,7 @@
 <?php require(APPROOT . '/views/includes/header.php') ?>
 <h3><?= $data['title'] ?></h3>
 
-
+<h4>TH-78-KL Ferrari</h4>
 <form action="<?= URLROOT ?>/mankementen/addMankement" method="post">
     <label for="mankement">Mankementen</label><br>
     <input type="text" name="mankement" id="mankement"><br>
